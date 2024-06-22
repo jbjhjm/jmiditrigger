d91841be82eb0da1ba5400049ee663a04b80a6d6
Jannik Mewes <mewes@yooniversity.com>
2024-06-22 15:56:59 +0200

    Update plugin version to 1.1.0

--- jmiditrigger/Source/Constants.h
+++ jmiditrigger/Source/Constants.h
@@ -8,7 +8,7 @@ const juce::String BUILD_TYPE = "Release";
 #endif
 
 
-const juce::String PLUGIN_VERSION = "1.0.1 " + BUILD_TYPE;
+const juce::String PLUGIN_VERSION = "1.1.0 " + BUILD_TYPE;
 const juce::String BUILD_DATE = __DATE__ " " __TIME__;
 
 const int MAX_LOG_LENGTH = 10000;
