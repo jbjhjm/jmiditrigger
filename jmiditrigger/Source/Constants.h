600a43088f8e920797681b3e1b2921aa0e0778bb
Jannik Mewes <mewes@yooniversity.com>
2024-06-22 17:13:41 +0200

    jmiditrigger2 versioning change to 2.x.x

--- jmiditrigger/Source/Constants.h
+++ jmiditrigger/Source/Constants.h
@@ -8,7 +8,7 @@ const juce::String BUILD_TYPE = "Release";
 #endif
 
 
-const juce::String PLUGIN_VERSION = "1.1.0 " + BUILD_TYPE;
+const juce::String PLUGIN_VERSION = "2.1.0 " + BUILD_TYPE;
 const juce::String BUILD_DATE = __DATE__ " " __TIME__;
 
 const int MAX_LOG_LENGTH = 10000;
